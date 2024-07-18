ff402e128237483f4b9f82a4d5e434391afffd36
Arun Thakur <athakur09@rediffmail.com>
2024-07-18 18:21:52 +0530

    structure change

--- pages/cancel.js
+++
@@ -1,9 +0,0 @@
-export default function Cancel() {
-    return (
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold">Payment Cancelled</h1>
-        <p>Your payment was cancelled. Please try again.</p>
-      </div>
-    );
-  }
-  
